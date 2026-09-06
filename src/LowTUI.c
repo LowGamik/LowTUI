2a8016109e7bfe23fe56b4edad74ea195f96603e
LowGamik <lowgamik@gmail.com>
2026-09-06 21:56:37 +0200

    added Raw Mode and setStyle for later

--- src/LowTUI.c
+++ src/LowTUI.c
@@ -1,5 +1,7 @@
-// CROSS PLATFORM INCLUDES
+// CROSS PLATFORM STANDARD INCLUDES
 #include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 // NATIVE PLATFORM INCLUDES
 #ifdef _WIN32
@@ -7,8 +9,12 @@
     #include <Windows.h>
 #else
     #include <termios.h>
+    #include <unistd.h>
+
+    struct termios orig_termios; 
 #endif
 
+// EVERYTHING FUCKING ELSE
 #include "LowTUI.h"
 
 void clearScreen(){
@@ -20,3 +26,39 @@ void resetMousePosition(){
     printf(ESCAPE_HOME_POSITION);
     fflush(stdout);
 }
+
+void sleepMS(size_t time){
+    #ifdef _WIN32
+        Sleep(time);
+    #else
+        usleep(time * 1000);
+    #endif
+}
+
+void setStyle(int foreGround, int backGround, bool isBold){
+    if(isBold){
+        printf("\033[1;%d;%dm", foreGround, backGround);
+    }else{
+        printf("\033[%d;%dm", foreGround, backGround);
+    }
+    fflush(stdout);
+}
+
+#ifdef _WIN32
+    void enableRawMode() {}
+    void disableRawMode() {}
+#else
+    void enableRawMode() {
+        struct termios raw;
+        tcgetattr(STDIN_FILENO, &orig_termios);
+        raw = orig_termios;
+        raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+        raw.c_cc[VMIN] = 1;
+        raw.c_cc[VTIME] = 0;
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    }
+
+    void disableRawMode() {
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    }
+#endif
--- src/LowTUI.h
+++ src/LowTUI.h
@@ -1,10 +1,18 @@
 #ifndef LOWTUI_H
 #define LOWTUI_H
 
+#include <stdbool.h>
+#include <stddef.h>
+
 #define ESCAPE_CLEAR_SCREEN "\033[2J"
-#define ESCAPE_HOME_POSITION "\022[H"
+#define ESCAPE_HOME_POSITION "\033[H"
+#define ESCAPE_RESET_STYLE "\033[0m"
 
 void clearScreen();
 void resetMousePosition();
+void sleepMS(size_t time);
+void setStyle(int foreGround, int backGround, bool isBold);
+void enableRawMode();
+void disableRawMode();
 
 #endif
